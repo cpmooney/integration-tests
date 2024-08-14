129b2e3fe44703d48e38e08c9b1e918cb81f4d0c
cpmooney <christopher.mooney.math@gmail.com>
2024-08-14 08:19:01 -0500

    Cleanup

--- src/test/java/com/moondog/labs/bible/BibleControllerTest.java
+++ src/test/java/com/moondog/labs/bible/BibleControllerTest.java
@@ -1,15 +1,13 @@
 package com.moondog.labs.bible;
 
-import static org.instancio.Select.field;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.instancio.Select.field;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class BibleControllerTest extends TestBase {
     @Test
